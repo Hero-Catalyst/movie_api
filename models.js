2d8261680750b1f14ae414ab7dd511c9bbc18caa
DistractedWanderer <smhall117@gmail.com>
2022-08-19 00:33:01 -0400

    Models.js update
    Added a models.js file for use with mongoose - created mongoose Schema for model use - created models - wrote export code for use in index.js

---
+++ models.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {type: mongoose.Schema.Types.ObjectId, ref: 'Genre'},
+  Director: {type: mongoose.Schema.Types.ObjectId, ref: 'Director'},
+  Actors: [{type: mongoose.Schema.Types.ObjectId, ref: 'Actor'}],
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let genreSchema = mongoose.Schema({
+  Name: {type: String, required: true},
+  Description: String
+});
+
+let directorSchema = mongoose.Schema({
+  Name: {type: String, required: true},
+  Bio: String,
+  Birth: Date,
+  Death: Date
+});
+
+let actorSchema = mongoose.Schema({
+  Name: {type: String, required: true},
+  Bio: String,
+  Birth: Date,
+  Death: Date
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+//Model Creation
+let Movie = mongoose.model('Movie', movieSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
+let Actor = mongoose.model('Actor', actorSchema);
+let User = mongoose.model('User', userSchema);
+
+//Model exports to index.js
+module.exports.Movie = Movie;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
+module.exports.Actor = Actor;
+module.exports.User = User;
