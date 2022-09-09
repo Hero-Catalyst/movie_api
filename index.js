0fdb5d4e5dde9511e24d19d80e0ff64dda09a544
DistractedWanderer <smhall117@gmail.com>
2022-09-09 13:49:07 -0400

    index update
    changed password back for error debugging

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
   Users.findOneAndUpdate({ Username: req.params.Username},
   { $set: {
     Username: req.body.Username,
-    Password: hashedPassword,
+    Password: req.body.Password,
     Email: req.body.Email,
     Birthday: req.body.Birthday
   }
