f0705d9ae817844ca32e0c6ab8c1cfa1aae1ccc7
DistractedWanderer <smhall117@gmail.com>
2022-09-14 13:30:49 -0400

    index update
    adding a get method to return users favorite movies

--- index.js
+++ index.js
@@ -164,6 +164,18 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }),
   });
 });
 
+// GET a users list of Favorite Movies
+app.get('/users/:Username/movies/:FavorieMovies', passport.authenticate('jwt', { session: false}),
+(req, res) => {
+  Users.findOne({ Username: req.params.Username})
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
 
 //GET a list of ALL movies
 app.get('/movies', passport.authenticate('jwt', {session: false}),
